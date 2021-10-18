3addb697caa30ae4a9533e3919388e1b4a8533f8
David Jablonski <dayjaby@gmail.com>
2021-10-18 17:55:46 +0200

    use fw_targetdir instead of hardcoded path

--- scripts/reset_firmware.sh
+++ scripts/reset_firmware.sh
@@ -4,13 +4,18 @@ set -e
 set -o nounset
 set -o pipefail
 
-FW_TARGETDIR=$(pwd)/firmware
+if [ -z "$FW_TARGETDIR" ]; then
+    FW_TARGETDIR=/home/beagle/Development/BeagleCompanion/firmware
+else
+    echo "Using firmware folder: $FW_TARGETDIR"
+fi
+
 PREFIX=$(ros2 pkg prefix micro_ros_setup)
 
 # Checking if firmware exists
 if [ -d $FW_TARGETDIR ]; then
     RTOS=$(head -n1 $FW_TARGETDIR/PLATFORM)
-    PLATFORM=$(head -n2 firmware/PLATFORM | tail -n1)
+    PLATFORM=$(head -n2 $FW_TARGETDIR/PLATFORM | tail -n1)
 else
     echo "Firmware folder not found. Please use ros2 run micro_ros_setup create_firmware_ws.sh to create a new project."
     exit 1
